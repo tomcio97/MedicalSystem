6c80865fc68f31f0bc455cc102dc6e8a0283af1e
= <twalasik97@gmail.com>
2020-07-03 15:13:29 +0200

    Initial commit - create project structure

---
+++ MedicalSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedicalSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
